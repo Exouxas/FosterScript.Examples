9193af2d55d4bfc643927991bb114786d7126ea8
Exouxas <exouxas@gmail.com>
2023-10-14 12:23:34 +0200

    Move circle creation outside the ctor

--- src/FosterScript.Examples.WpfExample/MainWindow.xaml.cs
+++ src/FosterScript.Examples.WpfExample/MainWindow.xaml.cs
@@ -38,34 +38,20 @@ namespace FosterScript.Examples
 
             _children = ActorCanvas.Children;
 
-            int count = 100;
-
-            for (int i = 0; i < count; i++)
-            {
-                Actor actor = CreateActor(world);
-
-                Ellipse circle = new();
-                circle.Width = 10;
-                circle.Height = 10;
-                circle.Fill = new SolidColorBrush(Color.FromRgb((byte)((255 / count) * (i + 1)), 0, 0));
-                circle.Stroke = Brushes.Black;
-                circle.StrokeThickness = 1;
-
-                _children.Add(circle);
-
-                actors.Add(actor, circle);
-
-                int r = 100;
-                double x = random.NextDouble() * r - r / 2d;
-                double y = random.NextDouble() * r - r / 2d;
-                actor.Move(new Vector3((float)x, (float)y, 0));
-                UpdatePosition(circle, x, y);
-
-            }
-
             world.StepDone += Tick;
             world.ActorMoved += OnActorMoved;
             world.ActorKilled += OnActorKilled;
+
+
+            int count = 100;
+            for (int i = 0; i < count; i++)
+            {
+                int r = 100;
+                double x = random.NextDouble() * r - r / 2d;
+                double y = random.NextDouble() * r - r / 2d;
+
+                Actor actor = CreateActor(world, x, y);
+            }
             
 
             world.Start();
@@ -119,7 +105,7 @@ namespace FosterScript.Examples
             });
         }
 
-        private Actor CreateActor(World world)
+        private Actor CreateActor(World world, double x, double y)
         {
             Actor actor = new(world);
             List<Module> modules = new();
@@ -143,6 +129,20 @@ namespace FosterScript.Examples
 
             actor.AddModule(modules);
             world.Add(actor);
+
+            Ellipse circle = new();
+            circle.Width = 10;
+            circle.Height = 10;
+            byte red = (byte)(255 * random.NextDouble());
+            byte green = (byte)(255 * random.NextDouble());
+            byte blue = (byte)(255 * random.NextDouble());
+            circle.Fill = new SolidColorBrush(Color.FromRgb(red, green, blue));
+            circle.Stroke = Brushes.Black;
+            circle.StrokeThickness = 1;
+
+            _children.Add(circle);
+            actors.Add(actor, circle);
+            actor.Move(new Vector3((float)x, (float)y, 0));
 
             return actor;
         }
