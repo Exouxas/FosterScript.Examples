3b1f1bea9a4811676d934339eb69921cf8654741
Exouxas <exouxas@gmail.com>
2023-10-15 18:37:31 +0200

    Fix private variable naming

--- src/AdvancedWpfExample/MainWindow.xaml.cs
+++ src/AdvancedWpfExample/MainWindow.xaml.cs
@@ -28,9 +28,9 @@ namespace FosterScript.Examples
     public partial class MainWindow : Window
     {
         private readonly UIElementCollection _children;
-        private readonly Random random = new();
-        private readonly Dictionary<Actor, Ellipse> actors = new();
-        private readonly IndefiniteWorld world = new(1000 / 60); // Set refreshrate to 60fps
+        private readonly Random _random = new();
+        private readonly Dictionary<Actor, Ellipse> _actors = new();
+        private readonly IndefiniteWorld _world = new(1000 / 60); // Set refreshrate to 60fps
 
         public MainWindow()
         {
@@ -38,47 +38,47 @@ namespace FosterScript.Examples
 
             _children = ActorCanvas.Children;
 
-            world.StepDone += Tick;
-            world.ActorMoved += OnActorMoved;
-            world.ActorKilled += OnActorKilled;
+            _world.StepDone += Tick;
+            _world.ActorMoved += OnActorMoved;
+            _world.ActorKilled += OnActorKilled;
 
 
             int count = 100;
             for (int i = 0; i < count; i++)
             {
                 int r = 100;
-                double x = random.NextDouble() * r - r / 2d;
-                double y = random.NextDouble() * r - r / 2d;
+                double x = _random.NextDouble() * r - r / 2d;
+                double y = _random.NextDouble() * r - r / 2d;
 
-                Actor actor = CreateActor(world, x, y);
+                Actor actor = CreateActor(_world, x, y);
             }
 
 
-            world.Start();
+            _world.Start();
         }
 
         private void OnActorMoved(Actor actor, Vector3 oldPosition, Vector3 newPosition)
         {
-            Ellipse circle = actors[actor];
+            Ellipse circle = _actors[actor];
 
             UpdatePosition(circle, newPosition.X, newPosition.Y);
         }
 
         private void OnActorKilled(Actor actor, Vector3 vector)
         {
-            Debug.WriteLine("Actor died! " + world.Actors.Count + " left");
+            Debug.WriteLine("Actor died! " + _world.Actors.Count + " left");
 
             // Remove circle from canvas
             Dispatcher.Invoke(() =>
             {
-                if (_children.Contains(actors[actor]))
+                if (_children.Contains(_actors[actor]))
                 {
-                    _children.Remove(actors[actor]);
+                    _children.Remove(_actors[actor]);
                 }
             });
 
             // Remove actor and circle from dictionary
-            actors.Remove(actor);
+            _actors.Remove(actor);
 
             // Create death marker
             Dispatcher.Invoke(() =>
@@ -90,9 +90,9 @@ namespace FosterScript.Examples
                 }
             });
 
-            if (world.Actors.Count == 0)
+            if (_world.Actors.Count == 0)
             {
-                world.Stop();
+                _world.Stop();
                 Debug.WriteLine("All actors died, stopped world");
             }
         }
@@ -124,17 +124,17 @@ namespace FosterScript.Examples
             List<Module> modules = new();
 
             SmartDigestion d = new();
-            d.DigestionRate = random.NextDouble() * 2;
-            d.StoredMeat = random.NextDouble() * 50;
-            d.StoredPlant = random.NextDouble() * 50;
+            d.DigestionRate = _random.NextDouble() * 2;
+            d.StoredMeat = _random.NextDouble() * 50;
+            d.StoredPlant = _random.NextDouble() * 50;
             modules.Add(d);
 
             SmartEnergy e = new();
-            e.EnergyStored = random.NextDouble() * 10;
+            e.EnergyStored = _random.NextDouble() * 10;
             modules.Add(e);
 
             SmartMovement2D mov = new();
-            mov.Speed = random.NextDouble() * 1 + 1;
+            mov.Speed = _random.NextDouble() * 1 + 1;
             modules.Add(mov);
 
             BasicBrain brain = new();
@@ -146,15 +146,15 @@ namespace FosterScript.Examples
             Ellipse circle = new();
             circle.Width = 10;
             circle.Height = 10;
-            byte red = (byte)(255 * random.NextDouble());
-            byte green = (byte)(255 * random.NextDouble());
-            byte blue = (byte)(255 * random.NextDouble());
+            byte red = (byte)(255 * _random.NextDouble());
+            byte green = (byte)(255 * _random.NextDouble());
+            byte blue = (byte)(255 * _random.NextDouble());
             circle.Fill = new SolidColorBrush(Color.FromRgb(red, green, blue));
             circle.Stroke = Brushes.Black;
             circle.StrokeThickness = 1;
 
             _children.Add(circle);
-            actors.Add(actor, circle);
+            _actors.Add(actor, circle);
             actor.Move(new Vector3((float)x, (float)y, 0));
 
             return actor;
